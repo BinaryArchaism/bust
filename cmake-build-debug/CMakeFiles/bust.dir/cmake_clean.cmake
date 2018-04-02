ef867686862b929d1088530accb0ff1c93df32b2
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2018-04-02 12:11:36 +0300

    awdawdfsfe

---
+++ cmake-build-debug/CMakeFiles/bust.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bust.dir/main.c.obj"
+  "bust.pdb"
+  "bust.exe"
+  "bust.exe.manifest"
+  "libbust.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bust.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int weight[] = {13, 8, 21, 14, 23, 16};
+    int n = sizeof(weight)/sizeof(weight[0]);
+    int count = 1;
+    for (int i = 0; i < n; i++) count = count*2;
+
+    int maxweight = 0;
+    int maxcount = 0;
+    int curweight = 0;
+    count--;
+
+    while (count >= 0) {
+        int copcount = count;
+        for (int i = n-1; i >= 0; i--){
+            int m = copcount & 1;
+            if (m == 1) curweight = curweight+weight[i];
+            copcount = copcount >> 1;
+        }
+        if (maxweight < curweight) {
+            maxcount = count;
+            maxweight = curweight;
+        }
+        count--;
+    }
+
+    printf("%i, %i", maxcount, maxweight);
     return 0;
 }
